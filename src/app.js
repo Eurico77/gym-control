530b62ed81cd50a9aabbbb4ec80317b09943cbb6
eurico77 <euricomagalhaes61@gmail.com>
2020-08-20 08:37:10 -0300

    reconfigurando

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
-const express = require('express')
-const app = express();
-const routes = require('./routes')
+const express = require("express");
+const routes = require("./routes");
+const nunjucks = require("nunjucks");
 
-app.use(routes)
+const app = express();
+
+app.use(routes);
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.set("view engine", "njk");
+nunjucks.configure("src/views", {
+	express: app,
+	autoescape: false,
+	noCache: true,
+});
+
+app.listen(8080, () => {
+  return console.log('rodando');
+});
 
 
 module.exports = app;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const routes = express.Router();
 
+routes.get('/', (req, res) => {
+  res.redirect('/instructors')
+})
 
+
+routes.get('/test', (req, res) => {
+  res.send("ola")
+})
+
+
+routes.get('/', (req, res) => {
+  res.render('/members')
+})
 
 
 module.exports = routes;
