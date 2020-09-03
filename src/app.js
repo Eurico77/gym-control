88043d182869ebdbe1fe9255cdbc25fb21d9d05f
eurico77 <euricomagalhaes61@gmail.com>
2020-09-02 21:11:25 -0300

    animações na tela de cadastro

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const routes = require("./routes");
 const nunjucks = require("nunjucks");
-
 const app = express();
 
 app.use(routes);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
+
 
 app.set("view engine", "njk");
 nunjucks.configure("src/views", {
